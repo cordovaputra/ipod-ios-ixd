3b3634efad5831113c2443cde00fd79d71577885
cordovaputra <cordovaputra.handri@gmail.com>
2023-12-12 16:58:29 +0700

    Initial Commit

---
+++ iPod-IXD/iPod_IXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPod_IXDApp.swift
+//  iPod-IXD
+//
+//  Created by Cordova  on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iPod_IXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
